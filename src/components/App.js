ae0fa0262f975940719d8c4c01c7b19b5dd94902
Arun <arunkumar7354861801@gmail.com>
2021-08-15 18:01:41 +0530

    fixed problem

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,36 @@
 import React, {Component, useState} from "react";
-import '../styles/App.css';
+
+function getCurrentTime(){
+    const currentDate=new Date();
+    return currentDate.toLocaleTimeString().toUpperCase();
+}
 
 class App extends Component {
+    constructor(props){
+        super(props);
+      //  this.currentDate=new Date();
+        this.state={
+            time:getCurrentTime(),
+        };
+
+    }
+    componentDidMount(){
+        this.intevalId=setInterval(()=>{
+            this.setState({time:getCurrentTime()});
+        },1000);
+    }
+    componentWillUnmount(){
+        clearInterval(this.intevalId);
+    }
+
     render() {
 
         return(
-            <>
+            <div className="Clock">
+                <h3 id="time">{this.state.time}</h3>
                
-            </>
-        )
+            </div>
+        );
     }
 }
 
